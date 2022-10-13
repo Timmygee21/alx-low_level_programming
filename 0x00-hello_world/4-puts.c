284ea8426749b5c87a816c4aeb1659e62ba93194
Timmygee21 <saytimmy@gmail.com>
2022-10-13 15:46:04 -0700

    pain

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - prints "Programming is like building a multilingual puzzle
- *
- *   * @void: Function argument
- *
- *    * Return: 0 if the code runs successfuly. 1 otherwise
- *
- *     */
+ *main -Entry point
+ *Return: 0 (success)
+ */
 int main(void)
 
 {

f710a363775569cb636079587a2b6c6b636668ab
Timmygee21 <saytimmy@gmail.com>
2022-10-14 16:57:26 -0700

    train

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,22 +3,26 @@
 #include <time.h>
 
 /**
- *main - The entry point for the program
- *Return: The exit code
+ * main - The entry point for the program
+ * Return: The exit code
  */
 
 int main(void)
 {
 	int n, lastDigit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
-		printf("Last digit of %d is %d ", n, lastDigit);
+
+	printf("Last digit of %d is %d ", n, lastDigit);
+
 	if (lastDigit > 5)
 		puts("and is greater than 5");
 	else if (lastDigit < 6 && lastDigit != 0)
 		puts("and is less than 6 and not 0");
 	else
 		puts("and is 0");
+
 	return (0);
 }

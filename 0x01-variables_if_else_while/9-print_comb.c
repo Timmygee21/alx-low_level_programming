111528a0dafe7322bec6a12eedda9dd1c8b3a820
Timmygee21 <saytimmy@gmail.com>
2022-10-14 18:24:22 -0700

    brainy

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point for the program
+ * Return: The exit code
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	
+	}
+	putchar('\n');
+
+	return (0);
+}

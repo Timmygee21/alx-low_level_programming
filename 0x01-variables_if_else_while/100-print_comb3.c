624391136ab5e68fe87c9de45973946f4fbed2cb
Timmygee21 <saytimmy@gmail.com>
2022-10-14 22:21:17 -0700

    gave you in

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
-			
+
 			if (i < 8)
 			{
 				putchar(',');

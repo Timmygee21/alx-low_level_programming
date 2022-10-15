cb3b7740f7434fd86751aad9fd30d0638b6cf2b8
Timmygee21 <saytimmy@gmail.com>
2022-10-14 19:16:28 -0700

    heal me Lord

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point for the program
+ * Return: The exit code
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{											putchar('0' + i);
+			putchar('0' + j);
+													if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

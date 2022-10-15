ea392badd6b687771cf87b88d36e424f6f6611b9
Timmygee21 <saytimmy@gmail.com>
2022-10-14 19:36:24 -0700

    keep me Lord

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,11 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = i + 1; j < 10; j++)
-		{											putchar('0' + i);
+		{
+			putchar('0' + i);
 			putchar('0' + j);
-													if (i < 8)
+
+			if (i < 8)
 			{
 				putchar(',');
 				putchar(' ');

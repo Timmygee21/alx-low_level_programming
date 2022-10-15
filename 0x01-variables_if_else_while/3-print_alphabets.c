14c88547260cfc4f15595dfe0c0e267dd12f7304
Timmygee21 <saytimmy@gmail.com>
2022-10-14 17:33:45 -0700

    graded

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,15 @@ int main(void)
 {
 	char ch = 'a';
 	int i;
+
 	for (i = 0; i < 26; i++)
 		putchar(ch + i);
+
 	ch = 'A';
 	for (i = 0; i < 26; i++)
 		putchar(ch + i);
+
 	putchar('\n');
+
 	return (0);
 }

5230ce8df304dde94559cd4373e299f874b15dfb
Timmygee21 <saytimmy@gmail.com>
2022-10-14 17:48:27 -0700

    hail

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,17 +10,18 @@
 int main(void)
 {
 	int n, lastDigit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
-		printf("Last digit of %d is %d ", n, lastDigit);
+
+	printf("Last digit of %d is %d ", n, lastDigit);
 
 	if (lastDigit > 5)
 		puts("and is greater than 5");
 
 	else if (lastDigit < 6 && lastDigit != 0)
 		puts("and is less than 6 and not 0");
-
 	else
 		puts("and is 0");
 

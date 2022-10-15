f6bb4c1261ff67b0fb3a837365e87d17840e1d3b
Timmygee21 <saytimmy@gmail.com>
2022-10-14 18:55:01 -0700

    learn it well

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - The entry point for the program
  * Return: The exit code
  */
+
 int main(void)
 {
-	int n, lastDigit;
+	char ch = 'a';
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	for (i = 0; i < 26; i++)
+	{
+		char value = ch + i;
 
-	printf("Last digit of %d is %d ", n, lastDigit);
+		if (value == 'q' || value == 'e')
+			continue;
+		putchar(value);
+	}
 
-	if (lastDigit > 5)
-		puts("and is greater than 5");
-	else if (lastDigit < 6 && lastDigit != 0)
-		puts("and is less than 6 and not 0");
-	else
-		puts("and is 0");
+	putchar('\n');
+
 	return (0);
 }

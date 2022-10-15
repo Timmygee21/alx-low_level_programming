d23fbd967d4022e954ce388ed6365bba866b27c3
Timmygee21 <saytimmy@gmail.com>
2022-10-14 23:18:21 -0700

    hear me now

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,10 +6,10 @@
   * Description: prints all possible combination of 2-digit numbers
   * Return: Always 0 (success)
   */
+
 int main(void)
 {
 	int c, i, k, j;
-
 	for (c = 48; c <= 57; c++)
 	{
 		for (i = 48; i <= 57; i++)
@@ -18,22 +18,25 @@ int main(void)
 			{
 				for (j = 48; j <= 57; j++)
 				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+																					if (c + i + k + j == 227 && c == 57)
 					{
-						putchar(c);
-						putchar(i);
-						putchar(k);
-						putchar(j);
-
-						if (c + i + k + j == 227 && c == 57)
-						{
-						break;
-						}
-						else																{
-						putchar(',');
-						putchar(',');
-						}															}
-				}															}															}
+					break;
+					{
+					else;
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}															}
+		}
 	}
 	putchar('\n');
 	return (0);

b23e0a577e08d14de0ca3c32c8de4e6ad3550453
Timmygee21 <saytimmy@gmail.com>
2022-10-15 19:53:49 -0700

    keeping

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 
 /**
- ** main - Entry point
- ** Description: prints all possible combination of 2-digit numbers
- ** Return: Always 0 (success)
- **/
+*main - Entry point
+*Description: prints all combination of digit numbers
+*Return: Always 0 (success)
+**/
 
 int main(void)
 {

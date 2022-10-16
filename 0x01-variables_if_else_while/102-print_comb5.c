c10c54e403240b37b25ede2c4adfcf430714ff8c
Timmygee21 <saytimmy@gmail.com>
2022-10-16 05:07:08 -0700

    lessof you

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -27,13 +27,10 @@ int main(void)
 						putchar(' ');
 						putchar(k);
 						putchar(j);
-
 						if (c + i + k + j == 227 && c == 57)
 						{
-
 							break;
 						}
-
 						else
 						{
 							putchar(',');

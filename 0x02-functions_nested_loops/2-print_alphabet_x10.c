f348a4285111ec3286723ada73aef3176b5c89b7
Timmygee21 <saytimmy@gmail.com>
2022-10-18 19:21:55 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,8 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_puthcar('\n');
+		_putchar('\n');
 	}
 }

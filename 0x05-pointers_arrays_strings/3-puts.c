f2346a5d4caa2856ae4d43925efeaa16629d021b
Timmygee21 <saytimmy@gmail.com>
2022-10-24 08:49:51 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a newline, to stdout
+ * _puts - prints a string, followed by a newline to stdout
  * @str: string parameter to print
  * Return: nothing
  */

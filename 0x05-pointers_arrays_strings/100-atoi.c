e4f00bbff4e56b7be8dc5b56d022d6921c354877
Timmygee21 <saytimmy@gmail.com>
2022-10-24 11:09:12 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,26 @@
 #include "main"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string input parameter
- * Return: converted integer from string
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: The integervalue of the converted string.
  */
 
 int _atoi(char *s)
 {
-	unsigned int num = 0;
 	int sign = 1;
+	unsigned int num = 0;
 	
 	do {
 		if (*s == '-')
 			sign *= -1;
+		
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
+		
 		else if (num > 0)
 			break;
 	} while (*s++);
+	
 	return (num * sign);
 }

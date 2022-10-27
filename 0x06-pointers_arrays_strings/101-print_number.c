73942d361fa8a255f0648b9182ba1d83cd3bb3e2
Timmygee21 <saytimmy@gmail.com>
2022-10-26 19:14:59 -0700

    rask 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,7 @@
 
 /**
   * print_number - print numbers chars
-  * @n: integer params
+  * @n: integer parameter
   * Return: 0
   **/
 
@@ -12,13 +12,11 @@ void print_number(int n)
 
 	n1 = n;
 	if (n < 0)
-
 	{
 		_putchar('-');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
-
 	{
 		print_number(n1 / 10);
 	}

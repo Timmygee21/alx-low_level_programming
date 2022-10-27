e977f5f9e91cf76aa438615d04ced997e345ad84
Timmygee21 <saytimmy@gmail.com>
2022-10-26 19:33:36 -0700

    task 9

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,8 +9,11 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
+char *leet(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+int _putchar(char c);
+int main(void);
 
 #endif

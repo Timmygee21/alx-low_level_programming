50b48e693aa9ab337fdcdefb13f2b48b92b55f70
Timmygee21 <saytimmy@gmail.com>
2022-12-05 16:53:13 -0800

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,14 @@
-#ifndef IO_
-#define IO_
-int _putchar(char c);
-int _strlen(const char *s);
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 #endif

26d5b141d633e1c79d63b2d62d1a2f30824f39ec
root <saytimmy@gmail.com>
2023-01-17 08:21:13 +0000

    initial commit

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,28 @@
+#ifndef HOLBERTON_H 
+ #define HOLBERTON_H 
+  
+ #include <stdio.h> 
+ #include <stdlib.h> 
+  
+ int _putchar(char c); 
+ int _islower(int c); 
+ int _isalpha(int c); 
+ int _abs(int n); 
+ int _isupper(int c); 
+ int _isdigit(int c); 
+ int _strlen(char *s); 
+ void _puts(char *s); 
+ char *_strcpy(char *dest, char *src); 
+ int _atoi(void); 
+ char *_strcat(char *dest, char *src); 
+ char *_strncat(char *dest, char *src, int n); 
+ char *_strncpy(char *dest, char *src, int n); 
+ int _strcmp(char *s1, char *s2); 
+ char *_memset(char *s, char b, unsigned int n); 
+ char *_memcpy(char *dest, char *src, unsigned int n); 
+ char *_strchr(char *s, char c); 
+ unsigned int _strspn(char *s, char *accept); 
+ char *_strpbrk(char *s, char *accept); 
+ char *_strstr(char *haystack, char *needle); 
+  
+ #endif /* HOLBERTON_H�/

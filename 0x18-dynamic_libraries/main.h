ba85940da1ab31fd30548c6360337e312301685d
Timmygee21 <saytimmy@gmail.com>
2023-01-26 17:27:08 +0100

    main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#ifndef HOLBERTON_H 
- #define HOLBERTON_H 
-  
- #include <stdio.h> 
- #include <stdlib.h> 
-  
- int _putchar(char c); 
- int _islower(int c); 
- int _isalpha(int c); 
- int _abs(int n); 
- int _isupper(int c); 
- int _isdigit(int c); 
- int _strlen(char *s); 
- void _puts(char *s); 
- char *_strcpy(char *dest, char *src); 
- int _atoi(void); 
- char *_strcat(char *dest, char *src); 
- char *_strncat(char *dest, char *src, int n); 
- char *_strncpy(char *dest, char *src, int n); 
- int _strcmp(char *s1, char *s2); 
- char *_memset(char *s, char b, unsigned int n); 
- char *_memcpy(char *dest, char *src, unsigned int n); 
- char *_strchr(char *s, char c); 
- unsigned int _strspn(char *s, char *accept); 
- char *_strpbrk(char *s, char *accept); 
- char *_strstr(char *haystack, char *needle); 
-  
- #endif /* HOLBERTON_H�/
-=======
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
@@ -53,4 +23,3 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
->>>>>>> 73eba353c0841c8f9d651b44fad4aa5d24a1c5d7
